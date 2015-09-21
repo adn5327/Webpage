54e4e16571a68ae178352b783355ff3d7b77abe4
angrave <angrave@illinois.edu>
2015-09-21 14:39:33 -0500

    L12

--- public/data/resources/CS241FA15-12-examples.c
+++ public/data/resources/CS241FA15-12-examples.c
@@ -10,16 +10,19 @@
 int counter; // global
 
 void* myfunc1(void*ptr) {
-
-   printf("My thread id is %ld and I’m starting at %d\n");
+  int value = * (int*)ptr;
+  printf("My thread id is %ld and I was given a pointer %p\n", (long) pthread_self(), ptr);
+   printf("My thread id is %ld and I’m starting at %d\n", (long) pthread_self(), value);
    return NULL;
 }
 
-int main() {
+int main1() {
    // Each thread gets a different value of i to process
    pthread_t tid; 
    for(int i =0; i < 10; i++) {
-      pthread_create(&tid, 0, myfunc1, &i);
+     int* params = malloc(sizeof(int));
+     *params = i;
+      pthread_create(&tid, 0, myfunc1, params);
   }
   sleep(5);
   return 0;
@@ -36,12 +39,22 @@ void*myfunc2(void*param) {
   return NULL;
 }
 
-int main2() {
+int main() {
   pthread_t tid1,tid2;
 pthread_create(&tid1, 0, myfunc2, NULL);
-pthread_create(&tid2, 0, myfunc2, NULL);
 pthread_join(tid1,NULL);
+pthread_create(&tid2, 0, myfunc2, NULL);
 pthread_join(tid2,NULL);
 printf("%d\n", counter2 );
 }
+
+
+
+
+
+
+
+
+
+
 
